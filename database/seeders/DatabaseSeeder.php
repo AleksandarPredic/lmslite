b820ecf23d22379fd62c0e8b7f7bf79564cf41c8
AleksandarPredic <aleksandar.predic@gmail.com>
2022-01-31 17:21:32 +0100

    Seed fix to remove users with password for password :)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,40 +26,41 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Create one admin user with multiple roles
-        $password = Hash::make('password');
-        $user = User::factory()->create([
-           'name' => 'Admin',
-           'email' => 'info@acapredic.com',
-           'password' => $password
-        ]);
+        if ('local' === config('app.env')) {
+            $password = Hash::make('password');
+            $user = User::factory()->create([
+                'name' => 'Admin',
+                'email' => 'info@acapredic.com',
+                'password' => $password
+            ]);
 
-        UserRole::create([
-            'role_id' => 1,
-            'user_id' => $user->id
-        ]);
+            UserRole::create([
+                'role_id' => 1,
+                'user_id' => $user->id
+            ]);
 
-        UserRole::create([
-            'role_id' => 2,
-            'user_id' => $user->id
-        ]);
+            UserRole::create([
+                'role_id' => 2,
+                'user_id' => $user->id
+            ]);
 
-        // Create guest user
-        $user = User::factory()->create([
-            'name' => 'Guest student',
-            'email' => 'guest@guest.local',
-            'password' => $password
-        ]);
+            // Create guest user
+            $user = User::factory()->create([
+                'name' => 'Guest student',
+                'email' => 'guest@guest.local',
+                'password' => $password
+            ]);
 
-        UserRole::create([
-            'role_id' => 3,
-            'user_id' => $user->id
-        ]);
+            UserRole::create([
+                'role_id' => 3,
+                'user_id' => $user->id
+            ]);
+        }
 
         // Create multi groups user - used below for calendar overrides
         $userMultiGroup = User::factory()->create([
             'name' => 'Multi group user',
             'email' => 'multi@group.local',
-            'password' => $password
         ]);
 
         UserRole::create([

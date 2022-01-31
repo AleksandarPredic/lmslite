d3bfb3a555dae29b33653182f6ef628a692a8839
AleksandarPredic <aleksandar.predic@gmail.com>
2022-01-31 17:05:21 +0100

    Event delete

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -130,7 +130,10 @@ class EventController extends Controller
      */
     public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return redirect(route('admin.events.index'))
+            ->with('admin.message.success', "Group, {$event->name}, deleted!");
     }
 
     /**

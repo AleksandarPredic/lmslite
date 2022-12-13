0cdc9eba70c2effc806f1a2cab52791770018b4a
aleksandarpredic <aleksandar.predic@gmail.com>
2022-12-13 17:33:08 +0100

    Table and starting logic for the statistics done

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -21,8 +21,12 @@ class StatisticsController extends Controller
          * http://localhost/admin/statistics
          */
 
+        // TODO: Check with client if we need to show the event name or group name
+
         // TODO: Add caching
-        $startDate = Carbon::createFromFormat('d/m/Y', '26/12/2022');
+
+        // TODO: use this for testing
+        $startDate = Carbon::createFromFormat('d/m/Y', '01/12/2022');
         $endDate = Carbon::createFromFormat('d/m/Y', '31/02/2023');
 
         $calenarEventUserStatuses = CalendarEventUserStatus::with('user')
@@ -49,42 +53,72 @@ class StatisticsController extends Controller
         foreach ($usersWithCalendarEventUserStatuses as $userId => $calendarEventUserStatuses) {
             $user = $calendarEventUserStatuses->first()->user;
 
-            // TODO: Use this for more detailed view when the user clicks on the table statuses
+            // Count all the statuses by month so we can group them after
             $statusesGroupedByMonth = [];
             foreach ($calendarEventUserStatuses as $calendarEventUserStatus) {
                 $statusesGroupedByMonth[] = [
                     'month' => $calendarEventUserStatus->calendarEvent->starting_at->format('m/Y'),
-                    'status' => $calendarEventUserStatus->status
+                    'status' => $calendarEventUserStatus->status,
+                    'calendarEventUserStatus' => $calendarEventUserStatus
                 ];
             }
 
-            $countStatusesPreMonth = [];
+            $sortedDataPerMonth = [];
             foreach ($statusesGroupedByMonth as $oneEventStatus) {
-                $countStatusesPreMonth[$oneEventStatus['month']]['status'][] = $oneEventStatus['status'];
+                $sortedDataPerMonth[$oneEventStatus['month']]['statuses'][] = $oneEventStatus['status'];
+                $sortedDataPerMonth[$oneEventStatus['month']]['calendarEventUserStatuses'][] = $oneEventStatus['calendarEventUserStatus'];
             }
 
             // If the user has no records for some months we have in the HTML table
             foreach (array_keys($datesWithKeysAsMonths) as $month) {
                 // Fill in empty columns to avoid having empty td in HTML table
                 // This will become below all zeros for statuses
-                if (! array_key_exists($month, $countStatusesPreMonth)) {
-                    $countStatusesPreMonth[$month]['status'] = [];
+                if (! array_key_exists($month, $sortedDataPerMonth)) {
+                    $sortedDataPerMonth[$month]['statuses'] = [];
+                    $sortedDataPerMonth[$month]['calendarEventUserStatuses'] = [];
                 }
             }
 
             $sortedUserStatuses[$userId] = (object)[
                 'user' => $user,
                 'months' => $statusesGroupedByMonth,
-                'countStatusesPreMonth' => array_map(function ($month) {
-                    $values = array_count_values($month['status']);
+                'sortedDataPerMonth' => array_map(function ($month) {
+                    $values = array_count_values($month['statuses']);
+
+                    // Re-map calendar event statuses by Event and by status
+                    $reMappedCalendarEventUserStatuses = [];
+                    if (! empty($month['calendarEventUserStatuses'])) {
+                        foreach ($month['calendarEventUserStatuses'] as $calendarEventUserStatus) {
+                            // Sort all by the same event id and the same status, but we will have to loop through it again below
+                            $reMappedCalendarEventUserStatuses[$calendarEventUserStatus->calendarEvent->event->id][$calendarEventUserStatus->status][] = (object)[
+                                'eventName' => $calendarEventUserStatus->calendarEvent->event->name,
+                                'status' => $calendarEventUserStatus->status,
+                                'userStatus' => $calendarEventUserStatus,
+                            ];
+                        }
+                    }
+
+                    // After sorting it, just remove status keys, so we can access the items more easily in te view
+                    $reMappedCalendarEventUserStatuses = array_map(
+                        fn($item) => array_values($item),
+                        $reMappedCalendarEventUserStatuses
+                    );
+
+                    //dd($reMappedCalendarEventUserStatuses);
+
                     return [
-                        'attended' => $values['attended'] ?? 0,
-                        'canceled' => $values['canceled'] ?? 0,
-                        'no-show' => $values['no-show'] ?? 0,
+                        'statuses' => [
+                            'attended' => $values['attended'] ?? 0,
+                            'canceled' => $values['canceled'] ?? 0,
+                            'no-show' => $values['no-show'] ?? 0,
+                        ],
+                        'sortedCalendarEventUserStatuses' => $reMappedCalendarEventUserStatuses
                     ];
-                }, $countStatusesPreMonth)
+                }, $sortedDataPerMonth)
             ];
         }
+
+       // dd(collect($sortedUserStatuses)->get(19));
 
         return view('admin.statistics.index', [
             'dates' => $datesWithKeysAsMonths,
--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -15,8 +15,55 @@
             table td {
                 width: 150px;
                 border: 1px solid;
+                text-align: center;
+            }
+
+            .statistics__details-month-trigger:hover {
+                color: blue;
+                font-weight: bold;
+            }
+
+            .statistics__details-month {
+                display: none;
+                position: fixed;
+                top: 50px;
+                left: 30px;
+                width: 300px;
+                height: auto;
+                background-color: white;
+            }
+
+            .statistics__details-month--show {
+                display: block;
             }
         </style>
+
+        <script>
+            window.addEventListener('load', () => {
+                const showCssClass = 'statistics__details-month--show';
+                const allModals = document.querySelectorAll('.statistics__details-month');
+
+                for (const modalTrigger of document.querySelectorAll('.statistics__details-month-trigger')) {
+                    modalTrigger.addEventListener('click', (event) => {
+                        event.preventDefault();
+
+                        for(const modal of allModals) {
+                            modal.classList.remove(showCssClass);
+                        }
+
+                        event.currentTarget.parentElement.nextElementSibling.classList.add(showCssClass);
+                    });
+                }
+
+                for (const modalClose of document.querySelectorAll('.statistics__details-month-close')) {
+                    modalClose.addEventListener('click', (event) => {
+                        event.preventDefault();
+
+                        event.currentTarget.parentElement.parentElement.classList.remove(showCssClass);
+                    });
+                }
+            });
+        </script>
 
         <table class="table-auto">
             <thead>
@@ -30,9 +77,61 @@
             <tbody>
                 @foreach($sortedUserStatuses as $sortedUserStatus)
                     <tr>
-                        <td>{{ $sortedUserStatus->user->name }} {{ $sortedUserStatus->user->id }}</td>
-                        @foreach($sortedUserStatus->countStatusesPreMonth as $monthPreview)
-                            <td>{{ $monthPreview['attended'] }} | {{ $monthPreview['canceled'] }} | {{ $monthPreview['no-show'] }}</td>
+                        <td>{{ $sortedUserStatus->user->name }}</td>
+                        @foreach($sortedUserStatus->sortedDataPerMonth as $monthPreview)
+                            <td>
+                                @php
+                                    $printStatuses = sprintf(
+                                        '%s | %s | %s',
+                                        $monthPreview['statuses']['attended'],
+                                        $monthPreview['statuses']['canceled'],
+                                        $monthPreview['statuses']['no-show']
+                                    );
+                                @endphp
+                                <div>
+                                    @if(! empty($monthPreview['sortedCalendarEventUserStatuses']))
+                                        <a class="statistics__details-month-trigger" href="#">{{ $printStatuses }}</a>
+                                    @else
+                                        {{ $printStatuses }}
+                                    @endif
+                                </div>
+
+                                @if(! empty($monthPreview['sortedCalendarEventUserStatuses']))
+                                    <div class="statistics__details-month border border-indigo-400 px-4">
+                                        <div class="mb-4 text-right">
+                                            <a class="statistics__details-month-close" href="#">X close</a>
+                                        </div>
+                                        <div class="statistics__details-month-inner">
+                                            {{-- Loop through each event --}}
+                                            @foreach($monthPreview['sortedCalendarEventUserStatuses'] as $sortedByEvent)
+                                                <strong><p class="mb-4">{{ $sortedByEvent[0][0]->eventName }}</p></strong>
+
+                                                {{-- Loop through each status --}}
+                                                @foreach($sortedByEvent as $sortedByStatus)
+                                                    <div class="bg-indigo-50 text-sm border border-gray-200 px-4 rounded text-center font-bold mb-2">
+                                                        {{ $sortedByStatus[0]->status }}
+                                                    </div>
+
+                                                    {{-- Loop through each status --}}
+                                                    <ul class="mb-4">
+                                                        @foreach($sortedByStatus as $calendarEventUserStatus)
+                                                            @php
+                                                                /**
+                                                                * @var \App\Models\CalendarEventUserStatus $status
+                                                                 */
+                                                                $status = $calendarEventUserStatus->userStatus
+                                                            @endphp
+                                                            <li class="text-sm">
+                                                                {{ $status->calendarEvent->starting_at->format('D, d.m.Y') }} - {{ $status->info ?? 'none'}}
+                                                            </li>
+                                                        @endforeach
+                                                    </ul>
+                                                @endforeach
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                @endif
+                            </td>
                         @endforeach
                     </tr>
                 @endforeach
